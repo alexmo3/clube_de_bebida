21ceadf7e669c52ea313ba33da2b4c9fea33bd6a
Phablo Raylan <phabloraylan@gmail.com>
2023-10-11 20:40:11 -0300

    init

---
+++ src/main/java/com/clubedebebida/backend/ClubeBebidaApplication.java
@@ -0,0 +1,13 @@
+package com.clubedebebida.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClubeBebidaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClubeBebidaApplication.class, args);
+	}
+
+}
