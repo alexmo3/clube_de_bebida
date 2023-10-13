ed7c6232467f5cdaca35169ebe7f23b2f3a83e97
alexmo3 <alexandre.marq@gmail.com>
2023-10-13 09:16:39 -0300

    Atualização do CRUD de USUARIOS usando os conceitos visto em aula.

---
+++ src/main/java/com/clubedebebida/backend/controller/UserController.java
@@ -0,0 +1,62 @@
+package com.clubedebebida.backend.controller;
+
+import com.clubedebebida.backend.dto.UserDTO;
+import com.clubedebebida.backend.service.UserService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/usuario")
+public class UserController {
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService){
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<UserDTO>> findAll(
+            @PageableDefault (size=10, page=0, sort="name")Pageable pageable
+    )
+    {
+        Page<UserDTO> usersDTO = userService.findAll(pageable);
+
+        return ResponseEntity.ok(usersDTO);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDTO> findById(@PathVariable Long id){
+        UserDTO usersDTO = userService.findById(id);
+
+        return ResponseEntity.ok(usersDTO);
+    }
+
+    @PostMapping
+    public ResponseEntity<UserDTO> save(@Valid @RequestBody UserDTO userDTO){
+        UserDTO savedUsuario = userService.save(userDTO);
+
+        return new ResponseEntity<>(savedUsuario, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UserDTO> update(@PathVariable Long id, @RequestBody UserDTO userDTO){
+        UserDTO updatedUsuario = userService.update(id, userDTO);
+
+        return ResponseEntity.ok(updatedUsuario);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(Long id){
+        userService.delete(id);
+
+        return ResponseEntity.noContent().build();
+    }
+}
