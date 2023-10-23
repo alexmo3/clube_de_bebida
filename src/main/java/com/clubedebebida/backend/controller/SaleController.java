b7a7cb748799c42f32bffaa2c92f499f7f421201
alexmo3 <alexandre.marq@gmail.com>
2023-10-23 06:29:22 -0300

    Criando VENDAS e CONSUMO

--- src/main/java/com/clubedebebida/backend/controller/SaleController.java
+++ src/main/java/com/clubedebebida/backend/controller/SaleController.java
@@ -12,7 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/venda")
+@RequestMapping("/vendas")
 public class SaleController {
     private final SaleService saleService;
 
